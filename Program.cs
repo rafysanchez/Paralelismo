3c13d3e53850b8c0c0ce1ada4374720cc600959e
diegostan <diego150691@gmail.com>
2022-01-18 06:54:56 -0300

    Publish

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using Paralelismo;
+using Paralelismo.Models;
+
+string[] ceps = new string[5];
+ceps[0] = "07155081";
+ceps[1] = "15800100";
+ceps[2] = "38407369";
+ceps[3] = "77445100";
+ceps[4] = "78015818";
+
+var parallelOptions = new ParallelOptions();
+parallelOptions.MaxDegreeOfParallelism = 8;
+
+var stopWatch = new Stopwatch();
+
+stopWatch.Start();
+var listaCep = new List<CepModel>();
+
+Parallel.ForEach(ceps, parallelOptions, cep =>
+{
+    listaCep.Add(new ViaCepService().GetCep(cep));
+});
+
+listaCep.OrderBy(cep => cep.Cep).ToList().ForEach(cep => Console.WriteLine(cep));
+
+
+stopWatch.Stop();
+
+Console.WriteLine($"O Tempo de processamento total é de {stopWatch.ElapsedMilliseconds} ms");
---
+++ ViaCepService.cs
@@ -0,0 +1,18 @@
+using System.Text.Json;
+using Paralelismo.Models;
+
+namespace Paralelismo
+{
+    public class ViaCepService
+    {
+        public CepModel GetCep(string cep)
+        {
+            var client = new HttpClient();
+            var response = client.GetAsync($"https://viacep.com.br/ws/{cep}/json/").Result;
+            var content = response.Content.ReadAsStringAsync().Result;
+            var cepResult = JsonSerializer.Deserialize<CepModel>(content);
+
+            return cepResult;            
+        }
+    }
+}
